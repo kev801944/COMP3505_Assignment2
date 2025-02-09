177672e62f86079f0becb5b8b6b10482d4fc436f
Kevin Luo <kevin801944@gmail.com>
2025-02-09 13:58:58 -0700

    Created RangeTest and set up test env

---
+++ JFreeChart/test/org/jfree/data/RangeTest.java
@@ -0,0 +1,13 @@
+package org.jfree.data;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class RangeTest {
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}

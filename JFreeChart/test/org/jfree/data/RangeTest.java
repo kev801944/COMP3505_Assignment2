ec17091d68c902e0413c152fc1b609e4db706e09
kev801944 <kevin801944@gmail.com>
2025-02-13 23:47:04 -0700

    Merge previous

--- JFreeChart/test/org/jfree/data/RangeTest.java
+++ JFreeChart/test/org/jfree/data/RangeTest.java
@@ -1,13 +1,31 @@
 package org.jfree.data;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class RangeTest {
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
-
+  	private Range exampleRange;
+  	@BeforeAll
+  	static void setUpBeforeClass() throws Exception {
+  	}
+  	@BeforeEach
+  	void setUp() throws Exception {
+  	}
+  	
+  	@Test                	
+  	void test() {
+  		exampleRange = new Range(-1,1);
+  		assertEquals(0, exampleRange.getCentralValue());
+  	}
+  	@AfterEach
+  	void tearDown() throws Exception {
+  	}
+  	@AfterAll
+  	static void tearDownAfterClass() throws Exception {
+  	}
+	
 }

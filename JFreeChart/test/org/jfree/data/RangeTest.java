0a80a69bcfeeb9e6704ed0bfb6155931b9387287
kev801944 <kevin801944@gmail.com>
2025-02-14 00:28:45 -0700

    Added test suite

--- JFreeChart/test/org/jfree/data/RangeTest.java
+++ JFreeChart/test/org/jfree/data/RangeTest.java
@@ -1,10 +1,7 @@
 package org.jfree.data;
 
 import static org.junit.jupiter.api.Assertions.*;
-<<<<<<< HEAD
-=======
 
->>>>>>> 9be3dd85afb42adbef7ee92f4fdb5b9524353a35
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -12,29 +9,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class RangeTest {
-<<<<<<< HEAD
   	private Range exampleRange;
-  	@BeforeAll
-  	static void setUpBeforeClass() throws Exception {
-  	}
-  	@BeforeEach
-  	void setUp() throws Exception {
-  	}
-  	
-  	@Test                	
-  	void test() {
-  		exampleRange = new Range(-1,1);
-  		assertEquals(0, exampleRange.getCentralValue());
-  	}
-  	@AfterEach
-  	void tearDown() throws Exception {
-  	}
-  	@AfterAll
-  	static void tearDownAfterClass() throws Exception {
-  	}
-	
-=======
-	private Range exampleRange;
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
@@ -58,5 +33,4 @@ class RangeTest {
  		assertEquals(0, exampleRange.getCentralValue(),0.1d,"The central value of (-1,1) is 0");
 	}
 
->>>>>>> 9be3dd85afb42adbef7ee92f4fdb5b9524353a35
 }
